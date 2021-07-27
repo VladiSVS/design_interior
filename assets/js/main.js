18d923b259349f7f373d360acbbdffca1923a7bb
VladiSVS <uladzimir.siauko@gmail.com>
2021-07-27 15:36:43 +0200

    design v1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function sendMessage(a, b, c) {
+    a = document.getElementById("name").value
+    b = document.getElementById("email").value
+    c = document.getElementById("message").value
+    console.log("Name:" + " " + a)
+    console.log("Email:" + " " + b)
+    console.log("Message:" + " " + c)
+}
